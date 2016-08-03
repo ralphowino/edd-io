5a8e6fa826f19864e868d34d6861472ec6db2be6
Raphael Owino <raphael@ralphowino.com>
2016-08-03 14:51:39 +0300

    partial commit - fix builder

--- components/output.js
+++ components/output.js
@@ -7,36 +7,51 @@ var
 class Output {
   line() {
     _.each(arguments, (message)=> {
+      if (!_.isString(message)) {
+        message = JSON.stringify(message);
+      }
       console.log(message);
     });
   };
 
   info() {
     _.each(arguments, (message)=> {
+      if (!_.isString(message)) {
+        message = JSON.stringify(message);
+      }
       console.info(chalk.cyan(message));
     });
   };
 
   success() {
     _.each(arguments, (message)=> {
+      if (!_.isString(message)) {
+        message = JSON.stringify(message);
+      }
       console.info(chalk.green(message));
     });
   };
 
   error() {
     _.each(arguments, (message)=> {
+      if (!_.isString(message)) {
+        message = JSON.stringify(message);
+      }
       console.error(chalk.white.bgRed(message));
     });
   };
 
   warning() {
     _.each(arguments, (message)=> {
+      if (!_.isString(message)) {
+        message = JSON.stringify(message);
+      }
       console.warn(chalk.yellow(message));
     });
   };
 
-  spinner (message){
-    let spinner = new Spinner(message+'...%s');
+  spinner(message) {
+    let spinner = new Spinner(message + '...%s');
     spinner.setSpinnerString('|/-\\');
     return spinner;
   }
